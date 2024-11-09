80e7f387979b7a324e92cda8132e9525626efc8d
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2024-11-09 14:27:40 -0300

    config do projeto

---
+++ app/Tabs/SoftSkills/SoftSkills.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SoftSkills() {
+  return (
+    <View>
+      <Text>SoftSkills</Text>
+    </View>
+  )
+}
